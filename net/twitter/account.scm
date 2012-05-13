4bff9854005f8a1f6f6d14962440163524842c6a
Masahiro Hayashi <mhayashi1120@gmail.com>
2012-05-13 23:57:46 +0900

    fix unbounded _keys

--- net/twitter/account.scm
+++ net/twitter/account.scm
@@ -17,7 +17,8 @@
 
 (define (account-verify-credentials/sxml
          cred :key (include-entities #f)
-         (skip-status #f))
+         (skip-status #f)
+         :allow-other-keys _keys)
   (call/oauth->sxml cred 'get #`"/1/account/verify_credentials.xml" 
                     (api-params _keys include-entities skip-status)))
 
@@ -35,7 +36,8 @@
 (define (account-settings-update/sxml cred :key (trend-location-woeid #f)
                                       (sleep-time-enabled #f)
                                       (start-sleep-time #f) (end-sleep-time #f)
-                                      (time-zone #f) (lang #f))
+                                      (time-zone #f) (lang #f)
+                                      :allow-other-keys _keys)
   (call/oauth->sxml cred 'post "/1/account/settings.xml" 
                     (api-params _keys trend-location-woeid sleep-time-enabled
                                   start-sleep-time end-sleep-time time-zone lang)))
@@ -46,18 +48,17 @@
 (define (account-update-profile-image/sxml 
          cred file :key
          (include-entities #f)
-         (skip-status #f))
+         (skip-status #f)
+         :allow-other-keys _keys)
   (call/oauth-post->sxml
    cred #`"/1/account/update_profile_image.xml"
    `((image :file ,file))
    (api-params _keys include-entities skip-status)))
 
-(define (account-update-profile-background-image/sxml 
-         cred file :key
-         (tile #f)
-         (include-entities #f)
-         (skip-status #f)
-         (use #f))
+(define (account-update-profile-background-image/sxml
+         cred file :key (tile #f) (include-entities #f)
+         (skip-status #f) (use #f)
+         :allow-other-keys _keys)
   (call/oauth-post->sxml
    cred #`"/1/account/update_profile_background_image.xml"
    `((image :file ,file))
@@ -72,7 +73,8 @@
          (profile-sidebar-border-color #f)
          (profile-text-color #f)
          (include-entities #f)
-         (skip-status #f))
+         (skip-status #f)
+         :allow-other-keys _keys)
   (call/oauth->sxml
    cred 'post #`"/1/account/update_profile_colors.xml"
    (api-params _keys profile-background-color profile-text-color
@@ -84,7 +86,8 @@
          cred :key (name #f)
          (url #f) (location #f)
          (description #f)
-         (include-entities #f) (skip-status #f))
+         (include-entities #f) (skip-status #f)
+         :allow-other-keys _keys)
   (call/oauth->sxml
    cred 'post #`"/1/account/update_profile.xml"
    (api-params _keys name url location description
