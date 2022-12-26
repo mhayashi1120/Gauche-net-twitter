cb1d41acbb08e67766b4099e0a19c82d73d6823e
Masahiro Hayashi <mhayashi1120@gmail.com>
2022-12-26 17:12:46 +0900

    import local changes (maybe not yet working although)

--- net/twitter/media.scm
+++ net/twitter/media.scm
@@ -1,20 +1,28 @@
 (define-module net.twitter.media
   (extend net.twitter.base)
   (export
-   upload/json)
+   status/json upload/json)
    )
 (select-module net.twitter.media)
 
+;; TODO FIXME Seems not working yet
+(define (status/json cred media-id)
+  (call/oauth->json
+   cred 'uploading-status
+   #"/1.1/media/upload"
+   (api-params `(:command "STATUS") media-id)))
+
 (define (upload/json cred :key (media #f) (media-data #f)
                      :allow-other-keys _keys)
-  (let1 body (cond
-              [media
-               `((media ,media))]
-              [media-data
-               `((media_data ,media-data))]
-              [else
-               #f])
+  (let1 mime-part
+      (cond
+       [media
+        `((media :value ,media))]
+       [media-data
+        `((media_data :value ,media-data :content-transfer-encoding "base64"))]
+       [else
+        #f])
+
     (call/oauth-upload->json
-     cred #`"/1.1/media/upload"
-     body
-     (api-params _keys))))
+     cred #"/1.1/media/upload"
+     mime-part (api-params _keys))))
