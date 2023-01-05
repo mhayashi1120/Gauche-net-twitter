4faf0152da5635b289fcb2ec1f7d4c9f82fb81d0
Masahiro Hayashi <mhayashi1120@gmail.com>
2023-01-05 20:32:03 +0900

    improve test environment

--- test.scm
+++ test.scm
@@ -5,17 +5,22 @@
 (use gauche.process)
 (use gauche.test)
 
+(debug-print-width #f)
+
 (test-start "net.twitter")
 
 (use net.twitter)
 (test-module 'net.twitter)
 
+(define (%do script)
+  (do-process `(gosh ,@(map (^l (format "-I~a" l)) *load-path*) ,script)))
+
 (test* "Sub module"
        #t
-       (do-process `(gosh "./__tests__/module.scm")))
+       (%do "./__tests__/module.scm"))
 
 (test* "Snowflake module"
        #t
-       (do-process `(gosh "./__tests__/snowflake.scm")))
+       (%do "./__tests__/snowflake.scm"))
 
 (test-end :exit-on-failure #t)
