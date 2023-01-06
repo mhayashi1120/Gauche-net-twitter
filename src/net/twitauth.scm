a5ba328839a5466d310350d76027ede0f352cc7f
Masahiro Hayashi <mhayashi1120@gmail.com>
2023-01-06 15:39:01 +0900

    add persistent option to twitauth.scm

--- src/net/twitauth.scm
+++ src/net/twitauth.scm
@@ -3,10 +3,12 @@
 ;;;
 
 (use net.twitter.auth)
+(use net.twitter.auth.persistent)
 (use util.match)
+(use gauche.parseopt)
 
 (define (usage)
-  (print "Usage: gosh net/twitauth [consumer-key consumer-secret]")
+  (print "Usage: gosh net/twitauth [{-o | --output-file} FILE] [consumer-key consumer-secret]")
   (print "  Without arguments, it prompts to enter the application's")
   (print "  consumer-key and consumer-secret.")
   (print "  After it obtains a request token, it asks the user to")
@@ -15,26 +17,34 @@
   (print "  and access-token-secret, with consumer-key and consumer-secret,")
   (print "  to the stdout.  Copy them to your application settings to")
   (print "  to access Twitter by the application on behalf of the user.")
+  (print "  -o | --output-file : Save credential to the FILE.")
   (exit 0))
 
 (define (main args)
-  (match (cdr args)
-    [()
-     (display "Enter consumer key: ") (flush)
-     (let1 key (read-line)
-       (when (eof-object? key) (exit 1 "aborted."))
-       (display "Enter consumer secret: ") (flush)
-       (let1 secret (read-line)
-         (when (eof-object? secret) (exit 1 "aborted."))
-         (report (twitter-authenticate-client key secret))))]
-    [(key secret) (report (twitter-authenticate-client key secret))]
-    [_ (usage)])
+  (let-args (cdr args)
+      ([output-file "o|output-file=s"]
+       [_ "h|help" => (^[] (usage))]
+       . restargs)
+
+    (match restargs
+      [()
+       (display "Enter consumer key: ") (flush)
+       (let1 key (read-line)
+         (when (eof-object? key) (exit 1 "aborted."))
+         (display "Enter consumer secret: ") (flush)
+         (let1 secret (read-line)
+           (when (eof-object? secret) (exit 1 "aborted."))
+           (report (twitter-authenticate-client key secret) output-file)))]
+      [(key secret)
+       (report (twitter-authenticate-client key secret) output-file)]
+      [_ (usage)]))
   0)
 
-(define (report cred)
-  (print "(")
-  (print " (consumer-key        . \""(ref cred'consumer-key)"\")")
-  (print " (consumer-secret     . \""(ref cred'consumer-secret)"\")")
-  (print " (access-token        . \""(ref cred'access-token)"\")")
-  (print " (access-token-secret . \""(ref cred'access-token-secret)"\")")
-  (print ")"))
+(define (report cred :optional (file #f))
+  (cond
+   [file
+    (call-with-output-file file
+      (^p (print-credential cred p)))
+    (format #t "Save credential to ~s\n" file)]
+   [else
+    (print-credential cred)]))
